c3d30bf3125085182ccf4ab47226e010012bb74d
jiakaiwangCN <107176646+jiakaiwangCN@users.noreply.github.com>
2025-05-19 14:37:00 +0000

    Deploying to gh-pages from @ jiakaiwangCN/jiakaiwangCN.github.io@db4bc9990f9049a74024c3988a8ef05ed146ed99 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -147,17 +147,6 @@ ninja.data = [{
             window.location.href = "/blog/2020/random/";
           
         },
-      },{id: "post-vscode安装指南-csdn博客",
-        
-          title: 'VSCode安装指南-CSDN博客 <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "文章浏览阅读760次。VSCode安装指南这是一篇傻瓜式的VSCode安装指南，还包括如何安装Python扩展工具，使其可以运行Python，多图预警！~~VSCode简介VSCode全称是Visual Studio Code，是由Microsoft出品的新一代编辑器，同时丰富的扩展使得VSCode可以做到IDE能做的事情。下载VSCode安装包点击这个VSCode下载安装文件，如下图：下载的安装包文件如下图：安装VSCode按照下图的流程安装这里可以选择自定义安装位置注意，这里的最好选上，对新手友",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.csdn.net/buaa1406/article/details/106125011?spm=1001.2014.3001.5502", "_blank");
-          
-        },
       },{id: "post-慎",
         
           title: "慎",
@@ -167,72 +156,6 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2019/forget/";
-          
-        },
-      },{id: "post-win10环境下安装pytorch踩坑实录-csdn博客",
-        
-          title: 'Win10环境下安装pytorch踩坑实录-CSDN博客 <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "文章浏览阅读2.1w次，点赞5次，收藏24次。本文详述了在Windows 10环境下使用pip安装PyTorch及遇到的常见问题，包括错误的pip指令、pip版本过低、安装失败等，并分享了手动下载whl文件的成功安装经验。",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.csdn.net/buaa1406/article/details/83104158?spm=1001.2014.3001.5502", "_blank");
-          
-        },
-      },{id: "post-基于扩展c0文法的编译器设计-part3-csdn博客",
-        
-          title: '基于扩展C0文法的编译器设计（Part3）-CSDN博客 <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "文章浏览阅读5.5k次，点赞3次，收藏2次。代码main.cpp#include #include #include #include #include #include &quot;asm.cpp&quot;#define after_switch 1using namespace std ;int main(){    string fileNam_c0文法",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.csdn.net/buaa1406/article/details/56667985?spm=1001.2014.3001.5502", "_blank");
-          
-        },
-      },{id: "post-基于扩展c0文法的编译器设计-part2-四元式assign-addi-imul等等-csdn博客",
-        
-          title: '基于扩展C0文法的编译器设计（Part2）_四元式assign,addi,imul等等-CSDN博客 <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "文章浏览阅读4.2k次。2．类/方法/函数功能词法分析部分：                    函数名            功能                    Int isSpace（）等            判断相关字符种类                             void getch（）            读取一个字符_四元式assign,addi,imul等等",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.csdn.net/buaa1406/article/details/56666397?spm=1001.2014.3001.5502", "_blank");
-          
-        },
-      },{id: "post-基于扩展c0文法的编译器设计-part1-csdn博客",
-        
-          title: '基于扩展C0文法的编译器设计（Part1）-CSDN博客 <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "文章浏览阅读4.4k次。本文档详细介绍了基于扩展C0文法的编译器设计，涵盖了文法说明、目标代码解释及初步的优化方案。内容包括文法规则，如加法、乘法和关系运算符，标识符、数字和字符定义，以及程序结构。此外，还涉及到常量和变量声明，函数定义，以及表达式和语句的解析。目标代码以MIPS指令形式表示，并进行了窥孔优化和全局变量替换等基础优化。",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.csdn.net/buaa1406/article/details/56666239?spm=1001.2014.3001.5502", "_blank");
-          
-        },
-      },{id: "post-词法分析程序实践-csdn博客",
-        
-          title: '词法分析程序实践-CSDN博客 <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "文章浏览阅读1.9k次，点赞2次，收藏7次。这是笔者按照课本中的思路实现的词法分析程序，具体细节和意义请参考 《编译技术》张莉等著.–北京：高等教育出版社,2016.9.ISBN: 978-7-04-046317-0 注：本程序使用文件读入的方法进行字符的读取，每次识别一个词，返回值一个词的类型，类型码在注释中给出。 代码如下：#include #include #include",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.csdn.net/buaa1406/article/details/52749906?spm=1001.2014.3001.5502", "_blank");
-          
-        },
-      },{id: "post-关于关系代数中除法的理解以及如何用基本运算表示除法-关系代数除法怎么理解-csdn博客",
-        
-          title: '关于关系代数中除法的理解以及如何用基本运算表示除法_关系代数除法怎么理解-CSDN博客 <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "文章浏览阅读2.2w次，点赞22次，收藏36次。本文详细解析了关系代数中的除法运算，并通过基本运算推导出除法的具体步骤。介绍了除法的定义及如何利用投影、笛卡尔积等操作实现除法运算。",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.csdn.net/buaa1406/article/details/52614461?spm=1001.2014.3001.5502", "_blank");
           
         },
       },{id: "books-the-godfather",
