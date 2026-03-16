6c64318014523ff4f496878b67df7c6b477dac92
jiakaiwangCN <107176646+jiakaiwangCN@users.noreply.github.com>
2026-03-16 01:30:51 +0000

    Deploying to gh-pages from @ jiakaiwangCN/jiakaiwangCN.github.io@e9b5b7d9514c2734a717d0c3113b19421806ad56 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "post-贺新郎",
+        },{id: "post-间歇性迷茫",
+        
+          title: "间歇性迷茫",
+        
+        description: "我与我周旋久",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/missing/";
+          
+        },
+      },{id: "post-贺新郎",
         
           title: "贺新郎",
         
